fa3519272b9dafeb7bcb2ef76ada0628148d729b
Jacobi Matrix <jacobimatrix@users.noreply.github.com>
2021-03-30 00:24:12 +0200

    WIP

--- src/views/BarView.tsx
+++ src/views/BarView.tsx
@@ -1,53 +1,64 @@
-import * as React from 'react';
+import * as React from "react";
 import Clock from "./../core/Clock";
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps } from "react-router";
 import clock from "../resources/images/clock-64x64.png";
 
 export interface BarViewState {
-    timeText: string;
+  timeText: string;
 }
 
+export default class BarView extends React.Component<
+  RouteComponentProps,
+  BarViewState
+> {
+  private readonly _clock: Clock;
 
+  constructor(props: any) {
+    super(props);
 
-export default class BarView extends React.Component<RouteComponentProps, BarViewState> {
-
-    private readonly _clock: Clock;
-
-    constructor(props: any) {
-        super(props);
-
-        this.state = {
-            timeText: ""
-        }
-
-        this._clock = new Clock((time: string) => {
-            this.setState({ timeText: time });
-        });
-
-    }
-    
-    componentDidMount() {
-        this._clock.run();
-    }
-
-    componentWillUnmount() {
-        this._clock.stop();
-    }
-
-    onGotoFooClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        console.log("Navigating to Foo view");
-        this.props.history.push("/");
+    this.state = {
+      timeText: "",
     };
 
-    public render() {
-        return (
-            <div>
-                <img src={clock}/>
-                <h3>BAR</h3>
-                <p>What time is it?</p>
-                <p>{this.state.timeText}</p>
-                <button onClick={this.onGotoFooClick}>Go to Foo</button>
-            </div>
-        );
-    }
+    this._clock = new Clock((time: string) => {
+      this.setState({ timeText: time });
+    });
+  }
+
+  componentDidMount() {
+    this._clock.run();
+  }
+
+  componentWillUnmount() {
+    this._clock.stop();
+  }
+
+  onGotoFooClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("Navigating to Foo view");
+    let navigator = require("jzz");
+    navigator.requestMIDIAccess().then(function (midi) {
+      let elements = Array.from(midi.outputs.values());
+      elements.map((e) => {
+        if (e.name.includes("K8")) {
+          console.log(e);
+          e.send([144, 57, 100]);
+        }
+        console.log(e);
+      });
+    });
+    // ...
+    navigator.close();
+  };
+
+  public render() {
+    return (
+      <div>
+        <img src={clock} />
+        <h3>BAR</h3>
+        <p>What time is it?</p>
+        <p>{this.state.timeText}</p>
+        <button onClick={this.onGotoFooClick}>Go to Foo</button>
+      </div>
+    );
+  }
 }
