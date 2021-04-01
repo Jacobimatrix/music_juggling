01d1ea005bb1e2b79a9ca6af72b2e54ccd26ef88
Jacobi Matrix <jacobimatrix@users.noreply.github.com>
2021-04-01 23:49:32 +0200

    Add audio player and enable loading local files

--- src/main-process.ts
+++ src/main-process.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, protocol } from "electron";
 
 declare const ENVIRONMENT: String;
 
@@ -6,45 +6,47 @@ const IS_DEV = ENVIRONMENT == "development";
 const DEV_SERVER_URL = "http://localhost:9000";
 const HTML_FILE_PATH = "index.html";
 
-
 let win: BrowserWindow | null = null;
 
-
 function createWindow() {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+      webSecurity: false,
+    },
+  });
 
-    if (IS_DEV) {
-        win.loadURL(DEV_SERVER_URL);
-        win.webContents.openDevTools();
-    }
-    else {
-        win.loadFile(HTML_FILE_PATH);
-    }
+  if (IS_DEV) {
+    win.loadURL(DEV_SERVER_URL);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(HTML_FILE_PATH);
+  }
 
-
-    win.on("closed", () => {
-        win = null
-    })
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
 app.on("ready", () => {
-    createWindow();
+  protocol.registerFileProtocol("file", (request, callback) => {
+    const pathname = decodeURI(request.url.replace("file:///", ""));
+    callback(pathname);
+  });
+  createWindow();
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
-})
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
 
-app.on('activate', () => {
-    if (win === null) {
-        createWindow()
-    }
-})
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
+});
